6df1317d05f9b593ddb6c9cc28fe13f085efb74a
Rahman <rahmanromli@gmail.com>
2019-10-31 23:55:59 +0800

    Flutter in Practice - E07: Automatic JSON Serialization

--- lib/Message.dart
+++ lib/Message.dart
@@ -1,10 +1,13 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'Message.g.dart';
+
+@JsonSerializable(nullable: false)
 class Message {
   final String subject;
   final String body;
 
   Message(this.subject, this.body);
 
-  Message.fromJson(Map<String, dynamic> json):
-    subject = json['subject'],
-    body = json['body'];
+  factory Message.fromJson(Map<String, dynamic> json) => _$MessageFromJson(json);
 }
