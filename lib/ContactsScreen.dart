b159922aae1dd3d10ebd68ccda341bc4867c487d
Rahman <rahmanromli@gmail.com>
2019-11-02 19:09:16 +0800

    Flutter in Practice - E18: Streams, StreamBuilder & BLoC

--- lib/ContactsScreen.dart
+++ lib/ContactsScreen.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_prct_yt/AppDrawer.dart';
+import 'package:flutter_prct_yt/ContactManager.dart';
 
 class ContactsScreen extends StatelessWidget {
+  ContactManager manager = ContactManager();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Contacts')),
+      appBar: AppBar(
+        title: Text('Contacts'),
+        actions: <Widget>[
+          Chip(
+            label: StreamBuilder<int>(
+              stream: manager.contactCounter,
+              builder: (context, snapshot) {
+                return Text(
+                  (snapshot.data ?? 0).toString(),
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  )
+                );
+              }
+            ),
+            backgroundColor: Colors.red,
+          ),
+          Padding(padding: EdgeInsets.only(right: 16)),
+        ],
+      ),
       drawer: AppDrawer(),
-      body: Center(
-        child: Text('Contacs'),
+      body: StreamBuilder<List<String>>(
+        stream: manager.contactListNow,
+        builder: (context, snapshot) {
+          List<String> contacts = snapshot.data;
+          return ListView.separated(
+            itemCount: contacts.length,
+            itemBuilder: (BuildContext context, int index){
+              return ListTile(title: Text(contacts[index]));
+            },
+            separatorBuilder: (context, index) => Divider(),
+          );
+        }
       ),
     );
   }
