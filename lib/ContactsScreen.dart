45a72664547fec166b818cfbca4143f1e7a4f13a
Rahman <rahmanromli@gmail.com>
2019-11-02 20:10:17 +0800

    Flutter in Practice - E19: Streams + Futures for HTTP Requests (Peek at RxVMS)

--- lib/ContactsScreen.dart
+++ lib/ContactsScreen.dart
@@ -2,47 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:flutter_prct_yt/AppDrawer.dart';
 import 'package:flutter_prct_yt/ContactManager.dart';
 
+import 'model/Contact.dart';
+
 class ContactsScreen extends StatelessWidget {
   ContactManager manager = ContactManager();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Contacts'),
-        actions: <Widget>[
-          Chip(
-            label: StreamBuilder<int>(
-              stream: manager.contactCounter,
+    return DefaultTabController(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Contacts'),
+          actions: <Widget>[
+            StreamBuilder<int>(
+              stream: manager.contactCount,
               builder: (context, snapshot) {
-                return Text(
-                  (snapshot.data ?? 0).toString(),
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  )
+                return Chip(
+                  label: Text(
+                    (snapshot.data ?? 0).toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    )
+                  ),
+                  backgroundColor: Colors.red,
                 );
-              }
+              },
             ),
-            backgroundColor: Colors.red,
-          ),
-          Padding(padding: EdgeInsets.only(right: 16)),
-        ],
+            Padding(padding: EdgeInsets.only(right: 16)),
+          ],
+        ),
+        drawer: AppDrawer(),
+        body: StreamBuilder(
+          stream: manager.contactListView,
+          builder: (BuildContext context, AsyncSnapshot<List<Contact>> snapshot) {
+            //
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+              case ConnectionState.active:
+                return Center(child: CircularProgressIndicator(),);
+              case ConnectionState.done:
+                List<Contact> contacts = snapshot.data;
+                return ListView.separated(
+                  itemCount: contacts?.length ?? 0,
+                  itemBuilder: (BuildContext context, int index){
+                    Contact _contact = contacts[index];
+                    return ListTile(
+                      title: Text(_contact.name),
+                      subtitle: Text(_contact.email),
+                      leading: CircleAvatar(),
+                    );
+                  },
+                  separatorBuilder: (context, index) => Divider(),
+                );
+            }
+          }
+        ),
       ),
-      drawer: AppDrawer(),
-      body: StreamBuilder<List<String>>(
-        stream: manager.contactListNow,
-        builder: (context, snapshot) {
-          List<String> contacts = snapshot.data;
-          return ListView.separated(
-            itemCount: contacts.length,
-            itemBuilder: (BuildContext context, int index){
-              return ListTile(title: Text(contacts[index]));
-            },
-            separatorBuilder: (context, index) => Divider(),
-          );
-        }
-      ),
+      length: 2,
     );
   }
 }
