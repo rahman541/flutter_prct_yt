41ec1f5e34cc12f165fec368ae8ec317eb6795f6
Rahman <rahmanromli@gmail.com>
2019-11-03 16:04:31 +0800

    EP31. use heroku

--- lib/service/ContactService.dart
+++ lib/service/ContactService.dart
@@ -4,7 +4,8 @@ import 'package:flutter_prct_yt/model/Contact.dart';
 import 'package:http/http.dart' as http;
 
 class ContactService {
-  static String _url = 'https://jsonplaceholder.typicode.com/users';
+  // static String _url = 'https://jsonplaceholder.typicode.com/users';
+  static String _url = 'https://secure-garden-21188.herokuapp.com/contacts';
   
   static Future<List<Contact>> browse({query}) async {
     http.Response response = await http.get(_url);
